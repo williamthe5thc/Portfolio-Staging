c913cf650aec1dad6b1436924fa9faffca3c8659
W. Jordan Charles <williamthe5thc@icloud.com>
2024-11-07 14:57:56 -0700

    Updates

--- src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// src/hooks/index.ts
-export * from './useFormValidation';
-export * from './useAnimationPresence';
-export * from './useModal';
-export * from './useIntersectionObserver';
--- src/types/gtag.d.ts
+++
@@ -1,9 +0,0 @@
-// src/types/gtag.d.ts
-declare global {
-  interface Window {
-    dataLayer: any[];
-    gtag: (...args: any[]) => void;
-  }
-}
-
-export {};
