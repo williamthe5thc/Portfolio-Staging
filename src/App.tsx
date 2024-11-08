7e713795295e06c3696cdb6e1b92b3bb3511e2ce
W. Jordan Charles <williamthe5thc@icloud.com>
2024-11-08 02:32:25 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// src/App.tsx 
+// src/App.tsx s
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AppProviders } from './providers/AppProviders';
